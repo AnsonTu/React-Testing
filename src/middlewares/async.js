7a12a540faafba9975f788d6cc5856d5d5e1a71b
Anson Tu <anson9137@gmail.com>
2019-07-29 16:50:55 -0400

    Added new file for middleware

---
+++ src/middlewares/async.js
@@ -0,0 +1,5 @@
+// Create a middleware function
+// dispatch
+// next is a reference to the next middleware in the middleware stack
+// action object returned by action creator, contains a type and possibly a payload
+export default ({ dispatch }) => next => action => {};
